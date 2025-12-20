3875b1abd013c780d62abd90ec67f0ea92c910ea
rgrjoy-prog23 <rgrjoy@gmail.com>
2025-12-20 14:30:14 +0530

    Advance Calculator part 1

--- src/components/AdvanceCalculator.tsx
+++ src/components/AdvanceCalculator.tsx
@@ -1,7 +1,51 @@
+import { Button } from "./shadcnui/button";
+import { Input } from "./shadcnui/input";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "./shadcnui/select";
+
 const AdvanceCalculator = () => {
-	return <div>Enter</div>;
+	return (
+		<div className="grid grid-cols-3 gap-8">
+			<Input
+				className="col-span-1"
+				placeholder="Number 1"
+			/>
+
+			<Select>
+				<SelectTrigger className="w-full">
 					<SelectValue placeholder="Operator" />
 				</SelectTrigger>
+				<SelectContent>
+					<SelectItem value="light">Light</SelectItem>
+					<SelectItem value="dark">Dark</SelectItem>
+					<SelectItem value="system">System</SelectItem>
+				</SelectContent>
+			</Select>
+
+			<Input
+				className="col-span-1"
+				placeholder="Number 2"
+			/>
+
+			<Input
+				className="col-span-2"
+				placeholder="Total"
+				disabled
+			/>
+
+			<Button className="col-span-1 cursor-pointer">Calculate</Button>
+			<Button
+				className="col-span-3 cursor-pointer"
+				variant={"destructive"}>
+				Reset
+			</Button>
+		</div>
+	);
 };
 
 export default AdvanceCalculator;
