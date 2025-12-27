300bd4b881c86b30e3dc92bf70d3136a2aba10c4
rgrjoy-prog23 <rgrjoy@gmail.com>
2025-12-27 14:04:21 +0530

    stat with input tag and function button

--- src/components/BasicCalculator.tsx
+++ src/components/BasicCalculator.tsx
@@ -1,31 +1,69 @@
+"use client";
+
 import { describe } from "node:test";
 import { Button } from "./shadcnui/button";
 import { Input } from "./shadcnui/input";
+import { useState } from "react";
 
 const BasicCalculator = () => {
+	const [inputOne, setInputOne] = useState("");
+	const [inputTwo, setInputTwo] = useState("");
+	const [inputTotal, setInputTotal] = useState("");
+
+	const hendleAdd = () => {};
+	const hendleSubtract = () => {};
+	const hendleMultiply = () => {};
+	const hendleDivide = () => {};
+	const hendleReset = () => {};
+
 	return (
 		<div className="grid grid-cols-4 gap-8">
 			<Input
 				className="col-span-2"
 				placeholder="Number 1"
+				onChange={({ target }) => setInputOne(target.value)}
+				value={inputOne}
 			/>
 			<Input
 				className="col-span-2"
 				placeholder="Number 2"
+				onClick={hendleSubtract}
+				onChange={({ target }) => setInputTwo(target.value)}
+				value={inputTwo}
 			/>
 			<Input
 				className="col-span-4"
 				placeholder="Total"
+				onClick={hendleMultiply}
+				onChange={({ target }) => setInputTotal(target.value)}
+				value={inputTotal}
 				disabled
 			/>
 
-			<Button className="col-span-1 cursor-pointer">Add</Button>
-			<Button className="col-span-1 cursor-pointer">Subtract</Button>
-			<Button className="col-span-1 cursor-pointer">Multiply</Button>
-			<Button className="col-span-1 cursor-pointer">Divide</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				onClick={hendleAdd}>
+				Add
+			</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				onClick={hendleSubtract}>
+				Subtract
+			</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				onClick={hendleMultiply}>
+				Multiply
+			</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				onClick={hendleDivide}>
+				Divide
+			</Button>
 			<Button
 				className="col-span-4 cursor-pointer"
-				variant={"destructive"}>
+				variant={"destructive"}
+				onClick={hendleReset}>
 				Reset
 			</Button>
 		</div>
