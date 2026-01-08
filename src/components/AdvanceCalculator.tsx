b99118906d897c8a51221894e8da3b2c30b8413c
rgrjoy-prog23 <rgrjoy@gmail.com>
2026-01-08 12:43:54 +0530

    BasicCalculator done and  AdvanceCalculator part done

--- src/components/AdvanceCalculator.tsx
+++ src/components/AdvanceCalculator.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { DivideIcon, MinusIcon, PlusIcon, X } from "lucide-react";
 import { Button } from "./shadcnui/button";
 import { Input } from "./shadcnui/input";
@@ -8,16 +10,38 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "./shadcnui/select";
+import { useState } from "react";
+import { Value } from "@radix-ui/react-select";
 
 const AdvanceCalculator = () => {
+	const [inputOne, setInputOne] = useState("");
+	const [inputTwo, setInputTwo] = useState("");
+	const [inputTotal, setInputTotal] = useState("");
+
+	const [inputOp, setInputOp] = useState("");
+
+	const hendleCalculate = () => {};
+
+	const hendleReset = () => {
+		setInputOne("");
+		setInputTwo("");
+		setInputTotal("");
+		setInputOp("");
+	};
+
 	return (
 		<div className="grid grid-cols-3 gap-8">
 			<Input
+				type="number"
 				className="col-span-1"
 				placeholder="Number 1"
+				onChange={({ target }) => setInputOne(target.value)}
+				value={inputOne}
 			/>
 
-			<Select>
+			<Select
+				onValueChange={(Value) => setInputOp(Value)}
+				value={inputOp}>
 				<SelectTrigger className="w-full">
 					<SelectValue placeholder="Operator" />
 				</SelectTrigger>
@@ -41,20 +65,36 @@ const AdvanceCalculator = () => {
 			</Select>
 
 			<Input
+				type="number"
 				className="col-span-1"
 				placeholder="Number 2"
+				onChange={({ target }) => setInputTwo(target.value)}
+				value={inputTwo}
 			/>
 
 			<Input
 				className="col-span-2"
 				placeholder="Total"
 				disabled
+				value={inputTotal}
 			/>
 
-			<Button className="col-span-1 cursor-pointer">Calculate</Button>
+			<Button
+				className="col-span-1 cursor-pointer"
+				disabled={inputOne === "" || inputTwo === "" || inputOp === ""}
+				onClick={hendleCalculate}>
+				Calculate
+			</Button>
 			<Button
 				className="col-span-3 cursor-pointer"
-				variant={"destructive"}>
+				disabled={
+					inputOne === "" &&
+					inputTwo === "" &&
+					inputTotal === "" &&
+					inputOp === ""
+				}
+				variant={"destructive"}
+				onClick={hendleReset}>
 				Reset
 			</Button>
 		</div>
--- src/components/BasicCalculator.tsx
+++ src/components/BasicCalculator.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { describe } from "node:test";
 import { Button } from "./shadcnui/button";
 import { Input } from "./shadcnui/input";
 import { useState } from "react";
@@ -10,58 +9,118 @@ const BasicCalculator = () => {
 	const [inputTwo, setInputTwo] = useState("");
 	const [inputTotal, setInputTotal] = useState("");
 
-	const hendleAdd = () => {};
-	const hendleSubtract = () => {};
-	const hendleMultiply = () => {};
-	const hendleDivide = () => {};
-	const hendleReset = () => {};
+	const hendleAdd = () => {
+		const numberOne = parseInt(inputOne);
+		const numberTwo = parseInt(inputTwo);
+
+		const total = numberOne + numberTwo;
+
+		setInputTotal(total.toString());
+
+		setInputOne("");
+		setInputTwo("");
+	};
+
+	const hendleSubtract = () => {
+		const numberOne = parseInt(inputOne);
+		const numberTwo = parseInt(inputTwo);
+
+		const total = numberOne - numberTwo;
+
+		setInputTotal(total.toString());
+
+		setInputOne("");
+		setInputTwo("");
+	};
+
+	const hendleMultiply = () => {
+		const numberOne = parseInt(inputOne);
+		const numberTwo = parseInt(inputTwo);
+
+		const total = numberOne * numberTwo;
+
+		setInputTotal(total.toString());
+
+		setInputOne("");
+		setInputTwo("");
+	};
+
+	const hendleDivide = () => {
+		const numberOne = parseInt(inputOne);
+		const numberTwo = parseInt(inputTwo);
+
+		const total = numberOne / numberTwo;
+
+		setInputTotal(total.toString());
+
+		setInputOne("");
+		setInputTwo("");
+	};
+
+	const hendleReset = () => {
+		setInputOne("");
+		setInputTwo("");
+		setInputTotal("");
+	};
 
 	return (
 		<div className="grid grid-cols-4 gap-8">
 			<Input
-				className="col-span-2"
+				type="number"
 				placeholder="Number 1"
+				className="col-span-2"
 				onChange={({ target }) => setInputOne(target.value)}
 				value={inputOne}
 			/>
 			<Input
-				className="col-span-2"
+				type="number"
 				placeholder="Number 2"
+				className="col-span-2"
 				onClick={hendleSubtract}
 				onChange={({ target }) => setInputTwo(target.value)}
 				value={inputTwo}
 			/>
 			<Input
-				className="col-span-4"
 				placeholder="Total"
+				type="number"
+				className="col-span-4"
 				onClick={hendleMultiply}
-				onChange={({ target }) => setInputTotal(target.value)}
+				// onChange={({ target }) => setInputTotal(target.value)}
 				value={inputTotal}
 				disabled
 			/>
 
 			<Button
 				className="col-span-1 cursor-pointer"
+				disabled={inputOne === "" || inputTwo === ""}
 				onClick={hendleAdd}>
 				Add
 			</Button>
+
 			<Button
 				className="col-span-1 cursor-pointer"
+				disabled={inputOne === "" || inputTwo === ""}
 				onClick={hendleSubtract}>
 				Subtract
 			</Button>
+
 			<Button
 				className="col-span-1 cursor-pointer"
+				disabled={inputOne === "" || inputTwo === ""}
 				onClick={hendleMultiply}>
 				Multiply
 			</Button>
+
 			<Button
 				className="col-span-1 cursor-pointer"
+				disabled={inputOne === "" || inputTwo === ""}
 				onClick={hendleDivide}>
 				Divide
 			</Button>
+
 			<Button
 				className="col-span-4 cursor-pointer"
+				disabled={inputOne === "" && inputTwo === "" && inputTotal === ""}
 				variant={"destructive"}
 				onClick={hendleReset}>
 				Reset
